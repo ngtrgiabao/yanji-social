1a02e75b8ee35936915593adcbfe6ac5dc2aba6a
ngtrgiabaoB2012063 <ngtrgiabao251002@gmail.com>
2023-05-19 12:09:44 +0700

    update url frontend and  backend

--- frontend/src/constants/backend.url.constant.js
+++ frontend/src/constants/backend.url.constant.js
@@ -1,4 +1,5 @@
-const BASE_URL = "http://localhost:8000";
+// const BASE_URL = "http://localhost:8000";
+const BASE_URL = "https://yanji-social.onrender.com/";
 const SOCKET_URL = "http://localhost:9000";
 
 export { BASE_URL, SOCKET_URL };
