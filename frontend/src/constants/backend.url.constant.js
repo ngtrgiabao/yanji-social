1c68d5c8a565d5d1621200610591e2234a58f2a9
ngtrgiabaoB2012063 <ngtrgiabao251002@gmail.com>
2023-05-27 12:18:21 +0700

    testing deploy

--- frontend/src/constants/backend.url.constant.js
+++ frontend/src/constants/backend.url.constant.js
@@ -1,4 +1,7 @@
-const BASE_URL = "http://localhost:8000";
+// const BASE_URL = "http://localhost:8000";
 const SOCKET_URL = "http://localhost:9000";
+
+// DEPLOY ENV
+const BASE_URL = "https://yanji-social.onrender.com";
 
 export { BASE_URL, SOCKET_URL };
