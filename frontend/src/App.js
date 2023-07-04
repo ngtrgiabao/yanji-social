a6f2b876677944f78a810cd3e4a897d8894857e0
ngtrgiabaoB2012063 <ngtrgiabao251002@gmail.com>
2023-07-04 16:48:27 +0700

    add handle network

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import { Suspense, lazy, useEffect, useRef } from "react";
+import { Suspense, lazy, useEffect, useRef, useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { io } from "socket.io-client";
@@ -31,84 +31,112 @@ function App() {
         socketRef.current = io(SOCKET_URL);
     }, [SOCKET_URL]);
 
+    const [isNetworkWorking, setIsNetworkWorking] = useState(navigator.onLine);
+
+    useEffect(() => {
+        const handleOnline = () => {
+            setIsNetworkWorking(true);
+        };
+
+        const handleOffline = () => {
+            setIsNetworkWorking(false);
+        };
+
+        window.addEventListener("online", handleOnline);
+        window.addEventListener("offline", handleOffline);
+
+        return () => {
+            window.removeEventListener("online", handleOnline);
+            window.removeEventListener("offline", handleOffline);
+        };
+    }, [isNetworkWorking]);
+
     return (
         <div className="App">
-            <Routes>
-                <Route
-                    path="*"
-                    element={
-                        <Suspense fallback={<LoadingPage />}>
-                            <_404 />
-                        </Suspense>
-                    }
-                />
-                <Route
-                    path="/"
-                    element={
-                        <Suspense fallback={<LoadingPage />}>
-                            <Homepage socket={socket} />
-                        </Suspense>
-                    }
-                />
-                <Route
-                    path="/messages"
-                    element={
-                        <Suspense fallback={<LoadingPage />}>
-                            {currentUser ? <MessagesPage /> : <RegisterPage />}
-                        </Suspense>
-                    }
-                />
-                <Route
-                    path="/explore"
-                    element={
-                        <Suspense fallback={<LoadingPage />}>
-                            <ExplorePage />
-                        </Suspense>
-                    }
-                />
-                <Route
-                    // path={"/user" + `/${currentUser._id}`}
-                    path={"/user/:userID"}
-                    element={
-                        <Suspense fallback={<LoadingPage />}>
-                            <PersonalPage socket={socket} />
-                        </Suspense>
-                    }
-                />
-                <Route
-                    path="/notification"
-                    element={
-                        <Suspense fallback={<LoadingPage />}>
-                            <NotificationPage />
-                        </Suspense>
-                    }
-                />
+            {isNetworkWorking ? (
+                <Routes>
+                    <Route
+                        path="*"
+                        element={
+                            <Suspense fallback={<LoadingPage />}>
+                                <_404 />
+                            </Suspense>
+                        }
+                    />
+                    <Route
+                        path="/"
+                        element={
+                            <Suspense fallback={<LoadingPage />}>
+                                <Homepage socket={socket} />
+                            </Suspense>
+                        }
+                    />
+                    <Route
+                        path="/messages"
+                        element={
+                            <Suspense fallback={<LoadingPage />}>
+                                {currentUser ? (
+                                    <MessagesPage />
+                                ) : (
+                                    <RegisterPage />
+                                )}
+                            </Suspense>
+                        }
+                    />
+                    <Route
+                        path="/explore"
+                        element={
+                            <Suspense fallback={<LoadingPage />}>
+                                <ExplorePage />
+                            </Suspense>
+                        }
+                    />
+                    <Route
+                        // path={"/user" + `/${currentUser._id}`}
+                        path={"/user/:userID"}
+                        element={
+                            <Suspense fallback={<LoadingPage />}>
+                                <PersonalPage socket={socket} />
+                            </Suspense>
+                        }
+                    />
+                    <Route
+                        path="/notification"
+                        element={
+                            <Suspense fallback={<LoadingPage />}>
+                                <NotificationPage />
+                            </Suspense>
+                        }
+                    />
 
-                <Route
-                    path="/register"
-                    element={
-                        <Suspense fallback={<LoadingPage />}>
-                            <RegisterPage />
-                        </Suspense>
-                    }
-                />
-                <Route
-                    path="/login"
-                    element={
-                        <Suspense fallback={<LoadingPage />}>
-                            <LoginPage />
-                        </Suspense>
-                    }
-                />
-                <Route
-                    path="/logout"
-                    element={
-                        <Suspense fallback={<LoadingPage />}>
-                            <LoginPage />
-                        </Suspense>
-                    }
-                />
-            </Routes>
+                    <Route
+                        path="/register"
+                        element={
+                            <Suspense fallback={<LoadingPage />}>
+                                <RegisterPage />
+                            </Suspense>
+                        }
+                    />
+                    <Route
+                        path="/login"
+                        element={
+                            <Suspense fallback={<LoadingPage />}>
+                                <LoginPage />
+                            </Suspense>
+                        }
+                    />
+                    <Route
+                        path="/logout"
+                        element={
+                            <Suspense fallback={<LoadingPage />}>
+                                <LoginPage />
+                            </Suspense>
+                        }
+                    />
+                </Routes>
+            ) : (
+                <NetworkError />
+            )}
         </div>
     );
 }
--- frontend/src/pages/networkError/NetworkError.jsx
+++ frontend/src/pages/networkError/NetworkError.jsx
@@ -1,5 +1,34 @@
+import NETWORK_BG from "../../assets/background/network-error.webp";
+
+import { faWifi } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 const NetworkError = () => {
-    return <div>NetworkError</div>;
+    return (
+        <div
+            className="w-100 d-flex justify-content-center align-items-center bg-black fs-1 flex-column"
+            style={{
+                height: "100vh",
+            }}
+        >
+            <FontAwesomeIcon
+                icon={faWifi}
+                className="mb-4 text-white"
+                style={{
+                    fontSize: "5rem",
+                }}
+            />
+
+            <div
+                className="d-flex justify-content-center align-items-center bg-danger fw-bold text-white p-2 px-3 text-uppercase"
+                style={{
+                    borderRadius: "1rem",
+                }}
+            >
+                please check your network and reload this page :/
+            </div>
+        </div>
+    );
 };
 
 export default NetworkError;
