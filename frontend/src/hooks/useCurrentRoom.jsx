99b79eb586100e48941b4f9e4debf17331fef88e
ngtrgiabao <ngtrgiabao251002@gmail.com>
2024-03-06 14:26:17 +0700

    hotfix: message room

--- frontend/src/hooks/useCurrentRoom.jsx
+++ frontend/src/hooks/useCurrentRoom.jsx
@@ -2,6 +2,7 @@ import { useSelector } from "react-redux";
 
 const useCurrentRoom = () => {
   return useSelector((state) => {
+    console.log(state.room)
     return state.room.room?.currentRoom;
   });
 };
